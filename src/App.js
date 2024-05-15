26f43a485e26c873fdb72ce08f855ffcb6dabb0e
dieum512 <akonkwadieum222@gmail.com>
2024-05-16 00:47:10 +0200

    Import the Header into the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/menu/Navbar';
+import Header from './components/header/Header';
 import './App.css';
 
 const App = () => (
   <div>
     <Navbar />
+    <Header />
   </div>
 );
 
