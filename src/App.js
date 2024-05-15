3cbf82e227608f27f9e212e01160824d91ddbc85
dieum512 <akonkwadieum222@gmail.com>
2024-05-15 23:05:41 +0200

    Import the App CSS file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import Navbar from './components/menu/Navbar'
+import React from 'react';
+import Navbar from './components/menu/Navbar';
+import './App.css';
 
 const App = () => {
   return (
