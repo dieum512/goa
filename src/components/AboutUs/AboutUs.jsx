6cdca03284e402dfbb6f9ce06ff1b080383874be
dieum512 <akonkwadieum222@gmail.com>
2024-05-18 21:20:51 +0200

    Add the G letter into the About section

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import G from '../../assets/G.png'
 
 const AboutUs = () => {
   return (
     <div className='app__aboutus app__bg flex__center section__padding' id='about'>
-      
+      <div className="app__aboutus-overlay flex__center">
+        <img src={G} alt="G letter" />
+      </div>
     </div>
   )
 }
