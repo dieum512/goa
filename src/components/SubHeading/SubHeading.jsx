f7cb85fc998168a335d66161ef9be56991423950
dieum512 <akonkwadieum222@gmail.com>
2024-05-18 12:46:08 +0200

    Create the Subheading JSX

---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import spoon from '../../assets/spoon.svg';
+
+const SubHeading = ({title}) => {
+  return (
+    <div style={{marginBottom: '1rem'}}>
+      <p className="p__cormorant">{title}</p>
+      <img src={spoon} alt="spon" className="spoon__img" />
+    </div>
+  )
+}
+
+export default SubHeading
