7e9ea57edf79390e1ad99ed22f704d9b2d3b9e26
dieum512 <akonkwadieum222@gmail.com>
2024-05-18 12:56:55 +0200

    Fix some JSX linter errors

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import spoon from '../../assets/spoon.svg';
 
-const SubHeading = ({title}) => {
-  return (
-    <div style={{marginBottom: '1rem'}}>
-      <p className="p__cormorant">{title}</p>
-      <img src={spoon} alt="spon" className="spoon__img" />
-    </div>
-  )
-}
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__cormorant">{title}</p>
+    <img src={spoon} alt="spon" className="spoon__img" />
+  </div>
+);
 
-export default SubHeading
+export default SubHeading;
