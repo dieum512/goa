4bb6ea3249ebd509dac762768deecb064325ca3a
dieum512 <akonkwadieum222@gmail.com>
2024-05-18 21:07:28 +0200

    Create the about funtion

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+      
+    </div>
+  )
+}
+
+export default AboutUs
