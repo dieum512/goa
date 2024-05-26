a2ee08ce9d39bc06fc2b1ac6fbb7f07c13dafdf1
dieum512 <akonkwadieum222@gmail.com>
2024-05-26 22:18:45 +0200

    Create the fake data

---
+++ src/constants/data.js
@@ -0,0 +1,35 @@
+const leftDetails = [
+    {
+        title: 'Architecture',
+        description: 'Conception des plans et planification des structures innovantes et esthetiques...',
+    },
+    {
+        title: 'Immobiliers',
+        description: `Gestion et conseil pour faciliter l'achat, la vente ou la location de biens immobiliers`,
+    },
+    {
+        title: 'Batiments et travaux publics',
+        description: `Construcyions et entretiens des infractutures telles que des routes, de ponts, des barrages...`,
+    },
+    {
+        title: 'Vente de materiaux de construction',
+        description: `fourniture et livraison de materiaux de construction de haute qualite..`,
+    },
+];
+
+const rightDetails = [
+    {
+        title: `Electricite`,
+        description: `Installation et maintenance de systemes electriques...`
+    },
+    {
+        title: `Plomberie`,
+        description: `Installation, reparation et entretien de systemes de plomberie`,
+    },
+    {
+        title: `Assainissement urbain`,
+        description: `Conception et la mise en place de systemes d'assainissement`,
+    },
+];
+
+export default {leftDetails, rightDetails}
