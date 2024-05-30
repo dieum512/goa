e28ec0e4b5d0525a7eea66eed6398db256b9e4b1
dieum512 <akonkwadieum222@gmail.com>
2024-05-30 21:57:42 +0200

    Finish the services component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 import SubHeading from '../SubHeading/SubHeading'
+import {leftDetails, rightDetails} from '../../constants/data'
+import MenuItem from '../MenuItem/MenuItem'
+import servicesImage from '../../assets/services.png'
+import './Services.css'
 
 const Services = () => {
   return (
@@ -9,6 +13,35 @@ const Services = () => {
         <SubHeading title='Services that fit your need'/>
         <h1 className="headtext__cormorant">Nos services</h1>
       </div>
+
+      <div className="app__specialMenu-menu">
+        <div className="app__specialMenu-menu_wine flex__center">
+          <p className='app__specialMenu-menu_heading'>What we do</p>
+          <div className="app__specialMenu-menu_items">
+            {leftDetails.map((service, index) => (
+              // <p>{service.title}</p>
+              <MenuItem key={service.title + index} title={service.title} galery={service.galery} description={service.description}/>
+            ))}
+          </div>
+        </div>
+
+        <div className="app__specialMenu-menu_img">
+          <img src={servicesImage} alt="House for services" />
+        </div>
+
+        <div className="app__specialMenu-menu_cocktails flex__center">
+          <p className='app__specialMenu-menu_heading'>What we do</p>
+          <div className="app__specialMenu-menu_items">
+            {rightDetails.map((service, index) => (
+              <MenuItem key={service.title + index} title={service.title} galery={service.galery} description={service.description}/>
+            ))}
+          </div>
+        </div>
+
+      </div>
+        <div style={{marginTop: '15px'}}>
+          <a href="#home" className='custom__button'>Let's talk</a>
+        </div>
     </div>
   )
 }
