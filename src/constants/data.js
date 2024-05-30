4ba852e3e398f49b2ae0fc42fe9809fa37628a1e
dieum512 <akonkwadieum222@gmail.com>
2024-05-30 21:57:07 +0200

    Update the data with the galery button

--- src/constants/data.js
+++ src/constants/data.js
@@ -2,32 +2,39 @@ export const leftDetails = [
     {
         title: 'Architecture',
         description: 'Conception des plans et planification des structures innovantes et esthetiques...',
+        galery: 'Images',
     },
     {
         title: 'Immobiliers',
         description: `Gestion et conseil pour faciliter l'achat, la vente ou la location de biens immobiliers`,
+        galery: 'Images',
     },
     {
         title: 'Batiments et travaux publics',
         description: `Construcyions et entretiens des infractutures telles que des routes, de ponts, des barrages...`,
+        galery: 'Images',
     },
     {
         title: 'Vente de materiaux de construction',
         description: `fourniture et livraison de materiaux de construction de haute qualite..`,
+        galery: 'Images',
     },
 ];
 
 export const rightDetails = [
     {
         title: `Electricite`,
-        description: `Installation et maintenance de systemes electriques...`
+        description: `Installation et maintenance de systemes electriques...`,
+        galery: 'Images',
     },
     {
         title: `Plomberie`,
         description: `Installation, reparation et entretien de systemes de plomberie`,
+        galery: 'Images',
     },
     {
         title: `Assainissement urbain`,
         description: `Conception et la mise en place de systemes d'assainissement`,
+        galery: 'Images',
     },
 ];
