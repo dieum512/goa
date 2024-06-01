a12e23bc28a5050271143c9c08ef464eed0c4e7e
dieum512 <akonkwadieum222@gmail.com>
2024-06-01 17:53:38 +0200

    Put the image in the  section

--- src/components/Belief/Belief.jsx
+++ src/components/Belief/Belief.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import SubHeading from '../SubHeading/SubHeading';
-import BelifImage from "../../assets/belief.png"
+import BeliefImage from "../../assets/belief.png"
 
 const Belief = () => {
   return (
-    <div>
+    <div className='app__bg app__wrapper section__padding'>
+        <div className="app__wrapper_img app__wrapper_img-reverse">
+            <img src={BeliefImage} alt="Goa stuff in the meeting" />
+        </div>
       
     </div>
   )
