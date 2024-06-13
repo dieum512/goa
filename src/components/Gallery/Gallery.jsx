026dae852f56fbc2cb7e8d2bfb02ce0f805b0aa1
dieum512 <akonkwadieum222@gmail.com>
2024-06-13 07:09:48 +0200

    Import the photos for gallry into the JSX

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Gallery01 from '../../assets/gallery01.jpg'
+import Gallery02 from '../../assets/gallery02.jpg'
+import Gallery03 from '../../assets/gallery03.jpg'
+import Gallery04 from '../../assets/gallery04.jpg'
 import './Gallery.css'
 
 const Gallery = () => {
